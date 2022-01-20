87a8a2e875de511f85671c75a08c0f7e45f174dc
ajaytest9725 <97875788+ajaytest9725@users.noreply.github.com>
2022-01-20 17:09:12 +0530

    Update style.js

--- src/style.js
+++ src/style.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
-   backgroundColor: '#00000999'
+   backgroundColor: '#00000050'
   },
   mask: {
     flex: 1,
